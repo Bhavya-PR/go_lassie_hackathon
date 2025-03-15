73ed572211e448bf3ec189b815a932861c71b516
Bhavya P R <bhavyapandurangan494@gmail.com>
2025-03-15 11:13:55 +0530

    updated Payer.js

--- server/models/Payer.js
+++ server/models/Payer.js
@@ -1,41 +1,62 @@
-/**
- * Payer Model
- */
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
+const { sequelize } = require('../config/db');
 
-const Payer = sequelize.define('payer', {
+const Payer = sequelize.define('Payer', {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
   name: {
-    type: DataTypes.STRING(255),
+    type: DataTypes.STRING,
     allowNull: false
   },
-  display_name: {
-    type: DataTypes.STRING(255),
+  payer_id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  address_line_1: {
+    type: DataTypes.STRING,
     allowNull: true
   },
-  payer_group_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'payer_groups',  // Refers to table name
-      key: 'id'               // Refers to column name in referenced table
-    }
+  address_line_2: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  city: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  state: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  zip: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  website: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  source: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    comment: 'The source of this payer data (e.g., file name)'
   }
 }, {
   tableName: 'payers',
-  timestamps: false,  // No created_at or updated_at columns
-  indexes: [
-    {
-      unique: true,
-      fields: ['name', 'payer_group_id'],
-      name: '_payer_group_uc'
-    }
-  ]
+  timestamps: true,
+  underscored: true
 });
 
 module.exports = Payer;
