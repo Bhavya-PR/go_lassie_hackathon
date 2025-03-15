35ed7e83c151e7c3e8c86e70cfb51a48423d3318
Bhavya P R <bhavyapandurangan494@gmail.com>
2025-03-15 13:32:26 +0530

    updated db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,15 @@
-const { Pool } = require('pg');
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 require('dotenv').config();
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // Required for Neon's SSL connection
-  },
+const dbPath = path.join(__dirname, '../', process.env.DB_PATH || 'database/payerDB.sqlite');
+
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error('Error connecting to SQLite:', err.message);
+  } else {
+    console.log('Connected to SQLite database');
+  }
 });
 
-module.exports = pool;
+module.exports = db;
